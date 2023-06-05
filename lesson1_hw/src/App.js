20e292e7a218799bf6c6e21a539fae2261ff91cc
ZhumagulOnorova <zhumagul.onorova.kk@gmail.com>
2023-06-05 17:36:33 +0600

    I add first folder

---
+++ lesson1_hw/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hello from './component/Hello';
+import World from './component/World';
+const App = () => {
+    return (
+        <div>
+            <Hello/>
+            <World/>
+        </div>
+    );
+};
+
+export default App;
